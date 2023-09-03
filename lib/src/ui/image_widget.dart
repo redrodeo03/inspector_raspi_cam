5cac0aee3d2a106f05fff5e8bf074d261f6bdd51
Abhinov Pankaj <abhinovpankaj1@hotmail.com>
2023-09-04 00:55:20 +0530

    add caching in image list.

--- lib/src/ui/image_widget.dart
+++ lib/src/ui/image_widget.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
-Image networkImage(String? netWorkImageURL) {
+Widget networkImage(String? netWorkImageURL) {
   String imageURL = "";
   if (netWorkImageURL == null) {
     return Image.asset(
-      "assets/images/heroimage.png",
+      "assets/images/icon.png",
       fit: BoxFit.fill,
       width: double.infinity,
     );
@@ -13,34 +14,35 @@ Image networkImage(String? netWorkImageURL) {
     imageURL = netWorkImageURL;
   }
   if (imageURL.startsWith('http')) {
-    return Image.network(
-      imageURL, width: double.infinity,
-      fit: BoxFit.fill,
-      // When image is loading from the server it takes some time
-      // So we will show progress indicator while loading
-      loadingBuilder: (BuildContext context, Widget child,
-          ImageChunkEvent? loadingProgress) {
-        if (loadingProgress == null) return child;
-        return Center(
-          child: CircularProgressIndicator(
-            value: loadingProgress.expectedTotalBytes != null
-                ? loadingProgress.cumulativeBytesLoaded /
-                    loadingProgress.expectedTotalBytes!
-                : null,
-          ),
-        );
-      },
-      // When dealing with networks it completes with two states,
-      // complete with a value or completed with an error,
-      // So handling errors is very important otherwise it will crash the app screen.
-      // I showed dummy images from assets when there is an error, you can show some texts or anything you want.
-      errorBuilder: (context, exception, stackTrace) {
-        return Image.asset(
-          "assets/images/heroimage.png",
-          fit: BoxFit.fill,
-          width: double.infinity,
-        );
-      },
+    // return Image.network(
+    //   imageURL, width: double.infinity,
+    //   fit: BoxFit.fill,
+    //   // When image is loading from the server it takes some time
+    //   // So we will show progress indicator while loading
+    //   loadingBuilder: (BuildContext context, Widget child,
+    //       ImageChunkEvent? loadingProgress) {
+    //     if (loadingProgress == null) return child;
+    //     return Center(
+    //       child: CircularProgressIndicator(
+    //         value: loadingProgress.expectedTotalBytes != null
+    //             ? loadingProgress.cumulativeBytesLoaded /
+    //                 loadingProgress.expectedTotalBytes!
+    //             : null,
+    //       ),
+    //     );
+    //   },
+
+    //   errorBuilder: (context, exception, stackTrace) {
+    //     return Image.asset(
+    //       "assets/images/icon.png",
+    //       fit: BoxFit.fill,
+    //       width: double.infinity,
+    //     );
+    //   },
+    // );
+    return CachedNetworkImage(
+      placeholder: (context, url) => const CircularProgressIndicator(),
+      imageUrl: imageURL,
     );
   } else {
     return Image.file(
@@ -50,3 +52,22 @@ Image networkImage(String? netWorkImageURL) {
     );
   }
 }
+
+// Thumbnail thumbnailImage(String? imageURL, BuildContext context) {
+//   return Thumbnail(
+//     dataResolver: () async {
+//       return (await DefaultAssetBundle.of(context).load(imageURL as String))
+//           .buffer
+//           .asUint8List();
+//     },
+//     mimeType: 'image/png',
+//     widgetSize: 300,
+//     errorBuilder: (uildContext, Exception error) {
+//       return Image.asset(
+//         "assets/images/icon.png",
+//         fit: BoxFit.fill,
+//         width: double.infinity,
+//       );
+//     },
+//   );
+// }
